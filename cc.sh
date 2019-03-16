441be36bab470ee439cf9c989ea5e8aa2e8cc9fc
llyyss1988 <llyyss88@163.com>
2019-03-16 18:50:57 +0800

    Update cc.sh

--- cc.sh
+++ cc.sh
@@ -4,14 +4,15 @@ echo `pwd`
 path=(pwd)
 echo $path
 if [ -d "./ChakraCoreFiles" ]; then
-echo 'file have ...'
+    echo 'file have ...'
 else
-echo 'file no...'
+    echo 'file no...'
+    echo 'wget https://aka.ms/chakracore/cc_linux_x64_1_11_1'
+    wget https://aka.ms/chakracore/cc_linux_x64_1_11_1
+    echo 'tar xf cc_linux_x64_1_11_1'
+    tar xf cc_linux_x64_1_11_1
 fi
-echo 'wget https://aka.ms/chakracore/cc_linux_x64_1_11_1'
-wget https://aka.ms/chakracore/cc_linux_x64_1_11_1
-echo 'tar xf cc_linux_x64_1_11_1'
-tar xf cc_linux_x64_1_11_1
+echo 'ls ChakraCoreFiles/*'
 ls ChakraCoreFiles/*
 echo '---------------------------'
 ls
